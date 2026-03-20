7a12df9f37699c8ec53db584f034cc71d20de213
alma-lutas <almalutas14@gmail.com>
2026-03-20 10:27:03 -0400

    modify db access endpoint

--- CRAA-Backend/main.py
+++ CRAA-Backend/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
 
+from db import get_db
 app = FastAPI()
 
 # Allow React dev server
@@ -8,7 +11,15 @@ origins = [
     "http://localhost:5173",
 ]
 
+@app.get("/flights")
+async def get_flights(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(text("SELECT"))
+    
+    flights = result.mappings().all()
+    return {"flights": flights}
+
 app.add_middleware(
+
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
