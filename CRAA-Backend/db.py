cacbfdee0833e8165a1e198d49db517a6713e8df
alma-lutas <almalutas14@gmail.com>
2026-03-26 12:09:27 -0400

    update get_embed_token()

--- CRAA-Backend/db.py
+++ CRAA-Backend/db.py
@@ -1,10 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
-import ssl
-
-from supabase import create_client, Client
 
 '''
 Sets up access point for Supabase 
@@ -13,7 +9,6 @@ Sets up access point for Supabase
 # load variables from .env
 load_dotenv() 
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
 
@@ -22,23 +17,3 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 def get_supabase():
     return supabase
 
-
-# # create sll context variable
-# ssl_context = ssl.create_default_context()
-# # disable full certificate verification
-# ssl_context.check_hostname = False
-# ssl_context.verify_mode = ssl.CERT_NONE
-
-# engine = create_async_engine(
-#     DATABASE_URL,
-#     connect_args={"ssl": ssl_context},
-#     echo=True,
-# )
-
-# AsyncSessionLocal = sessionmaker(
-#     engine, class_=AsyncSession, expire_on_commit=False
-# )
-
-# async def get_db():
-#     async with AsyncSessionLocal() as session:
-#         yield session
--- CRAA-Backend/main.py
+++ CRAA-Backend/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import text
+# from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy import text
 from supabase import Client
 from db import get_supabase
-
 import ssl 
-
+import msal 
 
 app = FastAPI()
 
@@ -14,21 +13,36 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
 ]
+''' 
+TODO: PowerBI Embedded link
 
+Dependencies: MSAL, requests
+Purpose: Create PowerBI endpoint callable by frontend
+Output: json embedToken + embedURL 
+
+'''
+@app.get("/get-embed-token")
+def get_embed_token():
+    # initialize MSAL client
+    app_id = msal.ConfidentialClientApplication(CLIENT_ID, client_credential=None, authority=None, validate_authority=True, token_cache=None, http_client=None, verify=True, proxies=None, timeout=None, client_claims=None, app_name=None, app_version=None, client_capabilities=None, azure_region=None, exclude_scopes=None, http_cache=None, instance_discovery=None, allow_broker=None, enable_pii_log=None, oidc_authority=None)
+    # retrieve token as application using username and password [acquire_token_by_username_password()]
+    # not as secure as client credentials [acquire_token_for_client()] since the school does not grant access to Entra ID
+    token = app.acquire_token_for_client(scopes="")
+    # call PowerBI REST API using access token
+
+
+'''
+TODO: retrieve flight information from Supabase 
+
+Dependencies: Client
+Purpose: access flight info from flight_instances table  
+Output: 
+
+'''
 @app.get("/flights")
 def get_flights(supabase: Client = Depends(get_supabase)):
     res = supabase.table("flight_instances").select("*").execute()
     return res.data
-
-
-# @app.get("/flights")
-# async def get_flights(db: AsyncSession = Depends(get_db)):
-#     a = 10
-#     testing = "SELECT flight_id FROM flight_instances LIMIT " + str(a)
-#     result = await db.execute(text(testing))
-#     flights = result.mappings().all()
-    
-#     return {"flights": flights}
 
 app.add_middleware(
 
